6e5d0d1513568f47e47edae5032d3a4d52b597f8
Developer <Lepiozdyx@gmail.com>
2026-03-05 13:33:54 -0300

    add wvnw

---
+++ Molten Burst/MBInfrastructure/AppContentView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct AppContentView: View {
+    
+    @StateObject private var manager = AppStateManager()
+        
+    var body: some View {
+        Group {
+            switch manager.appState {
+            case .request:
+                LoadingView()
+                
+            case .support:
+                if let url = manager.networkManager.gameURL {
+                    WKWebViewManager(
+                        url: url,
+                        webManager: manager.networkManager
+                    )
+                } else {
+                    WKWebViewManager(
+                        url: NetworkManager.initialURL,
+                        webManager: manager.networkManager
+                    )
+                }
+                
+            case .loading:
+                PGMenuView()
+            }
+        }
+        .onAppear {
+            manager.stateRequest()
+        }
+    }
+}
+
+#Preview {
+    AppContentView()
+}
